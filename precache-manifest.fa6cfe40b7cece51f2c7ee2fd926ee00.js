d0273cb66ce817f846b165439f983f9e331f7c54
Kindgren <kindgren@outlook.com>
2023-07-14 22:48:07 +0200

    Updates

---
+++ precache-manifest.fa6cfe40b7cece51f2c7ee2fd926ee00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e98d300dde4790aa3f0c",
+    "url": "/pace/static/css/main.af329fba.chunk.css"
+  },
+  {
+    "revision": "e98d300dde4790aa3f0c",
+    "url": "/pace/static/js/main.e98d300d.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/pace/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "270850e1ea4063e75fc5",
+    "url": "/pace/static/js/2.270850e1.chunk.js"
+  },
+  {
+    "revision": "7c9ef9c32bf17f6bfc15",
+    "url": "/pace/static/js/runtime~main.7c9ef9c3.js"
+  },
+  {
+    "revision": "101bdb7901c0ce9d86c3acb80ab3c63e",
+    "url": "/pace/static/media/runner.101bdb79.jpg"
+  },
+  {
+    "revision": "1f1a28d35a577348545535fe4719a671",
+    "url": "/pace/index.html"
+  }
+];
